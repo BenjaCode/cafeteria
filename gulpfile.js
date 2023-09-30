f5d90bd043f75cd42f20e7dcc82b8fd39ecdd846
BenjaCode <lbenjajrcode@gmail.com>
2023-09-29 22:13:41 -0600

    Para comprimir CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 
 function css(done){
     src('src/scss/app.scss')
+        /* .pipe(sass({outputStyle: 'compressed'})) */
         .pipe(sass())
             .pipe(dest('build/css'));
     done();
